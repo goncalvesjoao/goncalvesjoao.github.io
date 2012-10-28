a1cfb6889b29187f4496453b024e11aa09dbb86c
João Gonçalves <goncalves.joao@gmail.com>
2012-10-28 23:49:24 +0000

    new layout

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,10 +1,4 @@
 $(function() {
-	$('.didnt_work').live('click', function(event) {
-    $(this).hide();
-		$(this).next().fadeIn('fast');
-		event.preventDefault();
-	});
-	
 	$("textarea.line").each(function(){
 		var this_id = $(this).attr('id');
 		if (this_id != '' && this_id != undefined) {
--- public/javascript/modes/shell.js
+++ public/javascript/modes/shell.js
@@ -13,14 +13,14 @@ CodeMirror.defineMode('shell', function(config) {
 
   // Keywords
   define('keyword', 'if then do else elif while until for in esac fi fin ' +
-    'fil done exit set unset export function install new');
+    'fil done exit set unset export function install new use create gemset');
 
   // Commands
   define('builtin', 'ab awk bash beep cat cc cd chown chmod chroot clear cp ' +
     'curl cut diff echo find gawk gcc get git grep kill killall ln ls make ' +
     'mkdir openssl mv nc node npm ping ps restart rm rmdir sed service sh ' +
     'shopt shred source sort sleep ssh start stop su sudo tee telnet top ' +
-    'touch vi vim wall wc wget who write yes zsh mate brew ruby rails gem rake rvm');
+    'touch vi vim wall wc wget who write yes zsh mate brew ruby rails gem rake rvm mysql_install_db launchctl open');
 
   function tokenBase(stream, state) {
 
